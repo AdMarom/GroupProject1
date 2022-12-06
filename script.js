3435e6ae4eee35ece98cacd894161d8c1acff238
AdMarom <adenamarom@gmail.com>
2022-12-05 18:24:19 -0800

    added ticketmaster api

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 
+var searchTheCity = document.querySelector('#citySearch');
+var searchButton = document.querySelector('#button');
+var eventContainer = document.querySelector('#event');
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+  
+    var citySearch = searchTheCity.value.trim();
+  
+    if (citySearch) {
+      getEvents(citySearch);
+  
+      eventContainer.textContent = '';
+      searchTheCity.value = '';
+    } else {
+      alert('Please enter a City');
+    }
+  };
+
+  var getEvents = function (events) {
+
+  }
+
+    $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=100&apikey=StOkfSjg5F7T3VntM28nRbxZCjxQgC2m",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                // Parse the response.
+                // Do other things.
+             },
+    error: function(xhr, status, err) {
+                // This time, we do not end up here!
+             }
+  });
 
